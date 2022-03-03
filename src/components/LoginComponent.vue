a0ba2bcbc79586d784c22fe98376af0c17aa3241
Ladislav Sokol <sokollac10@gmail.com>
2022-03-03 16:43:41 +0100

    register and login form created

--- src/components/LoginComponent.vue
+++ src/components/LoginComponent.vue
@@ -6,16 +6,75 @@ export default defineComponent({
   data() {
     return {
       count: 2,
+      username: '',
+      password: '',
     };
   },
   computed: {},
-  methods: {},
+  methods: {
+    login() {
+      console.log(this.username + this.password);
+    },
+  },
 });
 </script>
 
 <template>
-  <div>
-    <p @click="count++">Login</p>
-    <p>Count: {{ count }}</p>
-  </div>
+  <q-card class="card-login">
+    <q-card-section>
+      <div class="text-h4">Login</div>
+      <q-input
+        class="input-alignment"
+        outlined
+        v-model="username"
+        label="Username"
+      />
+      <q-input
+        class="input-alignment"
+        type="password"
+        outlined
+        v-model="password"
+        label="Password"
+      />
+      <section class="column input-alignment">
+        <q-btn
+          color="primary"
+          label="Login"
+          @click="login"
+          class="input-alignment"
+        />
+        <section class="row register-alignment justify-center">
+          <p>Dont have an accout? &nbsp;</p>
+          <a href=""> Sign up</a>
+        </section>
+      </section>
+    </q-card-section>
+  </q-card>
 </template>
+
+<style lang="scss">
+.card-login {
+  width: 35%;
+}
+
+.input-alignment {
+  margin-top: 20px;
+}
+
+.register-alignment {
+  margin-top: 10px;
+  display: flex;
+}
+
+@media (max-width: $breakpoint-sm-max) {
+  .card-login {
+    width: 50%;
+  }
+}
+
+@media (max-width: $breakpoint-xs-max) {
+  .card-login {
+    width: 80%;
+  }
+}
+</style>
--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -6,16 +6,116 @@ export default defineComponent({
   data() {
     return {
       count: 2,
+      firstname: '',
+      lastname: '',
+      nickname: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
     };
   },
   computed: {},
-  methods: {},
+  methods: {
+    register() {
+      console.log('registered');
+    },
+  },
 });
 </script>
-
 <template>
-  <div>
-    <p @click="increment">Register</p>
-    <p>Count: {{ count }}</p>
-  </div>
+  <q-card class="card-register">
+    <q-card-section>
+      <div class="text-h4">Register</div>
+      <section class="row justify-between name-row">
+        <q-input
+          class="input-alignment name-input-width"
+          outlined
+          v-model="firstname"
+          label="Firstname"
+        />
+        <q-input
+          class="input-alignment name-input-width"
+          outlined
+          v-model="lastname"
+          label="Lastname"
+        />
+      </section>
+
+      <q-input
+        class="input-alignment"
+        outlined
+        v-model="nickname"
+        label="Nickname"
+      />
+      <q-input
+        type="email"
+        class="input-alignment"
+        outlined
+        v-model="email"
+        label="E-mail"
+      />
+
+      <q-input
+        class="input-alignment"
+        type="password"
+        outlined
+        v-model="password"
+        label="Password"
+      />
+      <q-input
+        class="input-alignment"
+        type="password"
+        outlined
+        v-model="confirmPassword"
+        label="Confirm password"
+      />
+      <section class="column input-alignment">
+        <q-btn
+          color="primary"
+          label="Register"
+          @click="register"
+          class="input-alignment"
+        />
+        <section class="row register-alignment justify-center">
+          <p>Already have an accout? &nbsp;</p>
+          <a href=""> Sign in</a>
+        </section>
+      </section>
+    </q-card-section>
+  </q-card>
 </template>
+
+<style lang="scss">
+.card-register {
+  width: 35%;
+}
+
+.name-row {
+  margin-top: 20px;
+}
+
+.name-input-width {
+  width: 48%;
+}
+
+.input-alignment {
+  margin-top: 20px;
+}
+
+.register-alignment {
+  margin-top: 10px;
+  display: flex;
+}
+
+@media (max-width: $breakpoint-sm-max) {
+  .card-register {
+    width: 50%;
+  }
+}
+
+@media (max-width: $breakpoint-xs-max) {
+  .card-register {
+    width: 80%;
+  }
+}
+</style>
