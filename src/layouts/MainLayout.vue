e5ac8e4b21cebceb2c9f108e828a192e9d55bc8b
Ladislav Sokol <sokollac10@gmail.com>
2022-03-10 13:05:04 +0100

    chat added

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -6,9 +6,7 @@ export default {
   setup() {
     return {
       icon: ref(false),
-      notificationModel: ref('ON'),
-      status: ref('Online'),
-      name: ref('Ladislav Sokol')
+      message: ref(['as'])
     };
   },
   components: {cNavbar}
@@ -30,7 +28,26 @@ export default {
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <!-- <router-view /> -->
+       <div class="q-pa-md row justify-center">
+    <div style="width: 100%; max-width: 400px">
+      <q-chat-message
+        name="me"
+        :text="['hey, how are you?']"
+        sent
+      />
+      <q-chat-message
+        name="Lacinko"
+        :text="message"
+      />
+    </div>
+  </div>
+  <q-footer>
+        <q-toolbar class="bg-grey-3 text-black row">
+          <q-input rounded outlined dense class="WAL__field col-grow q-mr-sm" bg-color="white" v-model="message[0]" placeholder="Type a message" />
+          <q-btn round flat icon="send" />
+        </q-toolbar>
+      </q-footer>
     </q-page-container>
   </q-layout>
 </template>
