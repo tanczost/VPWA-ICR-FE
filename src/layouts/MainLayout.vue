62096bc64495766e7a32461a5598f5ae9dda2951
Patrik Podhorsky <patyo@eduroam-160-53.fiit.stuba.sk>
2022-03-10 13:05:42 +0100

    added channels

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -15,10 +15,39 @@
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <!-- drawer content -->
-      <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
+      <div class="text-h4 q-mt-sm">Channels</div>
+
+      <div class="q-pa-md" style="max-width: 350px">
+        <q-list bordered class="rounded-borders">
+          <q-expansion-item
+            expand-separator
+            icon="account_circle"
+            label="Private"
+          >
+            <div v-for="channelName in 50" :key="channelName">
+              <q-card>
+                <q-card-section bordered class="q-ma-sm">
+                  Drawer {{ channelName }} / 50
+                </q-card-section>
+              </q-card>
+            </div>
+          </q-expansion-item>
+
+          <q-expansion-item expand-separator icon="public" label="Public">
+            <div v-for="channelName in 50" :key="channelName">
+              <q-card>
+                <q-card-section bordered class="q-ma-sm">
+                  Drawer {{ channelName }} / 50
+                </q-card-section>
+              </q-card>
+            </div>
+          </q-expansion-item>
+        </q-list>
+      </div>
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <div class="text-h4 q-mt-sm">People</div>
       <div v-for="n in 50" :key="n">Drawer {{ n }} / 50</div>
       <!-- drawer content -->
     </q-drawer>
